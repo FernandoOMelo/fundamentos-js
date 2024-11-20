9b92de7e38a5bf6be65f182b3a983687197ca459
Fernando Melo <fernando.omelo@hotmail.com>
2024-11-20 18:46:19 -0300

    Aula 04
     - Operadores

---
+++ operadores-comparacao.js
@@ -0,0 +1,11 @@
+const estaAprovado = true;
+
+if (estaAprovado === true){
+    console.log('Aprovado');
+}
+
+if ('0' === 0){ //Convenção de utilizar sempre três iguais.
+    console.log('Passou na comparação');
+} else {
+    console.log('Não passou na comparação');
+}
