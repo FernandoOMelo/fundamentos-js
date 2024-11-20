40dfd47429c7af7731ae14b18064c1ea2681872f
Fernando Melo <fernando.omelo@hotmail.com>
2024-11-20 17:47:00 -0300

    Aula 03
    Tipo de dados - null

---
+++ tipo-null.js
@@ -0,0 +1,14 @@
+//Valor undefined
+let nomeEstudante;
+const professora = 'Dina';
+console.log(nomeEstudante);
+console.log(professora);
+console.log(typeof(professora));
+console.log(typeof(nomeEstudante));
+
+//Valor null
+let telefoneEstudante = null;
+console.log(telefoneEstudante);
+console.log(typeof(telefoneEstudante));
+console.log(telefoneEstudante + 3);
+console.log(nomeEstudante + 3);
