6a9cec5eeb06f1e6f5f85f0c6852a357ab4e0081
Fernando Melo <fernando.omelo@hotmail.com>
2024-11-20 17:38:54 -0300

    Aula 03
    Tipo de dados - Boolean

---
+++ tipo-boolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Fernando';
+const estaAprovado = true;
+
+if (estaAprovado){
+    console.log(`Parabéns ${estudante} você está aprovado!`);
+} else {
+    console.log(`Que pena ${estudante} você foi reprovado!`);
+}
+
+if (estudante === 'Fernando'){
+    console.log(`Olá ${estudante}`);
+} else {
+    console.log('Quem é você?')
+}
